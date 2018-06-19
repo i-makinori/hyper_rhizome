8dfda5633f36af29a539bf2f1b8b02b60a34a8e9
i-makinori <maau3p@gmail.com>
2018-06-19 09:23:27 +0900

    [Add] templates of model of world, dot contents not implemented

--- src/test.lisp
+++ src/test.lisp
@@ -1,4 +1,44 @@
 (in-package :society)
 
-(defun hoge ()
-  (+ 1 2))
+;;;; structures
+
+
+(defun map ()
+  ;; Cell agent
+)
+
+(defmacro defmaptile (show update)
+  ;; show :: IO String 
+  ;; update :: Tile -> [Tile] -> Tile
+)
+
+
+
+(defstruct agent-dna
+  
+)
+
+(defstruct agent
+  
+)
+
+
+(defstruct world
+  ()
+)
+
+
+;;;; times,spaces
+
+(defun update (world)
+)
+
+;;;; 
+(defun init-world (rule)
+)
+
+(defun init-state (rule)
+)
+
+(defun update-state (state)
+)
